45c547998cf60b5389ef96c7d729012bdaf3d035
Keddon Campbell <mrkfed101@gmail.com>
2015-02-07 19:35:36 -0500

    Testing
    Added variables yearBorn and myName to text the uses of javascript

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
  */
 alert("Hello There!");
 alert("This is a second alert test");
+
+var yearBorn = 1992;
+var myName = "Keddon";
+
+console.log(yearBorn);
+console.log(myName);
+
+alert(myname + "was born in" + yearBorn);
