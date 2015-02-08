85a1d01b3f4ccc6c532b7110b4dc4f6639f0be21
Keddon Campbell <mrkfed101@gmail.com>
2015-02-07 19:55:00 -0500

    Practise
    Tested the use of the ‘ vs “ in the script.js file

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@
 var age; //declaration
 age = 22; //definition
 
+var isStudent = false;
+var myName = "Keddon";
+
+var phrase = 'I don\'t know. \n You\'ll have to speak with the professor';
+
 //storage container = value
 
 var yearOfBirth =1992; //declaring and defining on the same line
 
-
+alert(myName + phrase);
 
 
 
@@ -34,7 +39,7 @@ var yearOfBirth =1992; //declaring and defining on the same line
 alert("This is a second alert test");
 
 var yearBorn = 1992;
-var myName = "Keddon";
+
 
 console.log(yearBorn);
 console.log(myName);
