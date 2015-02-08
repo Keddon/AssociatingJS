4755beb49e989ecfd92cafd3f043b19ffe9b7496
Keddon Campbell <mrkfed101@gmail.com>
2015-02-07 19:46:20 -0500

    Definitions
    Added the variable called age which had a value of 22.  Commented out
    unused code.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 /**
  * Created by keddoncampbell on 2/7/15.
  */
-alert("Hello There!");
+
+var age; //declaration
+age = 22; //definition
+
+//storage container = value
+
+var yearOfBirth =1992; //declaring and defining on the same line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* alert("Hello There!");
 alert("This is a second alert test");
 
 var yearBorn = 1992;
@@ -10,4 +39,5 @@ var myName = "Keddon";
 console.log(yearBorn);
 console.log(myName);
 
-alert(myname + "was born in" + yearBorn);
+alert(myname + "was born in" + yearBorn); ...*/
+
