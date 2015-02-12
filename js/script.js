84f1985ba1a5ad5a45e56be66a390bd24726283f
Keddon Campbell <mrkfed101@gmail.com>
2015-02-12 18:02:13 -0500

    Casting
    Casting the strings as numbers and vice versa.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,34 @@
  */
 
 
-var objective = "Concatenating Strings";
+var objective = "Casting";
 alert(objective);
 console.log(objective);
 
-var firstName = Keddon;
+
+
+
+var stringVar = "6";
+var result = 7 + stringVar
+console.log(result);
+
+var operation = "casting a string as a number";
+console.log(operation);
+var result = 7 + Number(stringVar);
+console.log(result);
+
+
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4528;
+var phoneNo = areaCode + firstPart+ secPart;
+console.log(phoneNo);
+var operation = "casting a numbers to strings";
+console.log(operation);
+var phoneNo = "(" +String(areaCode) +")"+ String(firstPart) +"-"+ String(secPart);
+console.log(phoneNo);
+
+/*var firstName = Keddon;
 var lastName = Campbell;
 var fullName = firstName + " " +lastName;
 console.log(fullName);
@@ -16,9 +39,6 @@ var a = "6";
 var b = "7";
 var results = a + b;
 console.log(results);
-
-
-
 
 /*var orangeBins = [234, 567, 882];
 var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
